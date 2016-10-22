2def26f5fff6e6f6305986f907a503fabe26836c
Oles Pisarenko <doctornkz@yandex.ru>
2016-10-23 01:52:30 +0300

    Dirty prototype. Something wrong with emulator. Need to check on real device 6.0.

--- app/src/main/java/local/doctor/blinker/MainActivity.java
+++ app/src/main/java/local/doctor/blinker/MainActivity.java
@@ -1,20 +1,30 @@
 package local.doctor.blinker;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
+import android.hardware.camera2.CameraManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText lenPause;
+    private EditText lenSignal;
+    private EditText numSignal;
+    private String TAG = "Blinker";
+    private Boolean existCamera = true;
+    private CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -22,31 +32,72 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                lenPause = (EditText)findViewById(R.id.lengthPause);
+                int lenPauseInt = Integer.parseInt(lenPause.getText().toString());
+                lenSignal = (EditText)findViewById(R.id.lenghtSignal);
+                int lenSignalInt = Integer.parseInt(lenSignal.getText().toString());
+                numSignal = (EditText)findViewById(R.id.numSignal);
+                int numSignalInt = Integer.parseInt(numSignal.getText().toString());
+                Log.d(TAG, "Timing detected:" + lenPause.getText().toString() + " " + lenSignal.getText().toString() + " " + numSignal.getText().toString());
+
+                if (existCamera) {
+                    try {
+                        for (int num = 0;  num < numSignalInt ; num++) {
+                            Log.d(TAG, "Starting in flashlight cycle. Sleeping " + lenPause.getText().toString() + " sec");
+                            Thread.sleep(lenPauseInt * 1000);
+                            Log.d(TAG, "Turning on flashlight for " + lenPause.getText().toString() + " sec ");
+                            manager.setTorchMode("0", true);
+                            Log.d(TAG, "Waiting with flashlight for " + lenSignal.getText().toString() + " sec ");
+                            Thread.sleep(lenSignalInt * 1000);
+                            Log.d(TAG, "Turning off flashlight. Cycle finished");
+                            manager.setTorchMode("0", false);
+                        }
+                        Log.d(TAG, "onStart:Camera switched ON by switch");
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                        Log.d(TAG, "onStart:Switch ON failed");
+                    }
+
             }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
+    });
 
-        return super.onOptionsItemSelected(item);
     }
+
+    public void onStart(){
+        super.onStart();
+
+
+
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "onPause:");
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+
+        Log.d(TAG, "onRestart:");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        Log.d(TAG, "onStop:");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        Log.d(TAG, "onDestroy:");
+    }
+
+
 }
